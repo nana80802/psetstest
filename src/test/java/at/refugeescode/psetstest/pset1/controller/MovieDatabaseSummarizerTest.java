b806d4ec6d7e676ac996e8a48d113d80d904c21b
nana80802 <miss_nisreen2000@yahoo.com>
2018-03-25 17:25:34 +0200

    make the code cleaner

--- src/test/java/at/refugeescode/psetstest/pset1/controller/MovieDatabaseSummarizerTest.java
+++ src/test/java/at/refugeescode/psetstest/pset1/controller/MovieDatabaseSummarizerTest.java
@@ -1,12 +1,15 @@
 package at.refugeescode.psetstest.pset1.controller;
 
 import at.refugeescode.psetstest.pset1.model.Summary;
-import org.junit.jupiter.api.Assertions;
 
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class MovieDatabaseSummarizerTest {
     MovieDatabaseSummarizer movieDatabaseSummarizer = new MovieDatabaseSummarizer();
+
     @org.junit.jupiter.api.Test
     void summarize() {
         Summary summary = movieDatabaseSummarizer.summarize();
@@ -17,30 +20,30 @@ class MovieDatabaseSummarizerTest {
         List<String> mostAppearingGenres = summary.getMostAppearingGenres();
         String maleFemaleRatio = summary.getMaleFemaleRatio();
 
-        Assertions.assertAll (
+        assertAll(
                 () -> {
-                    Assertions.assertEquals(Integer.valueOf(29947), numberOfMovies);
-                    Assertions.assertEquals(Integer.valueOf(274570), numberOfActors);
+                    assertEquals(Integer.valueOf(29947), numberOfMovies);
+                    assertEquals(Integer.valueOf(274570), numberOfActors);
 
-                    Assertions.assertEquals((5), mostRatedMovies.size() );
-                    Assertions.assertEquals("Reckless (10.0)", mostRatedMovies.get(0));
-                    Assertions.assertEquals("Girl in the Cadillac (10.0)",mostRatedMovies.get(1));
-                    Assertions.assertEquals("The Haunted World of Edward D. Wood, Jr. (10.0)", mostRatedMovies.get(2));
-                    Assertions.assertEquals("Carmen Miranda: Bananas Is My Business (10.0)", mostRatedMovies.get(3));
-                    Assertions.assertEquals("Other Voices Other Rooms (10.0)", mostRatedMovies.get(4));
+                    assertEquals((5), mostRatedMovies.size());
+                    assertEquals("Reckless (10.0)", mostRatedMovies.get(0));
+                    assertEquals("Girl in the Cadillac (10.0)", mostRatedMovies.get(1));
+                    assertEquals("The Haunted World of Edward D. Wood, Jr. (10.0)", mostRatedMovies.get(2));
+                    assertEquals("Carmen Miranda: Bananas Is My Business (10.0)", mostRatedMovies.get(3));
+                    assertEquals("Other Voices Other Rooms (10.0)", mostRatedMovies.get(4));
 
-                    Assertions.assertEquals(5, mostHiredActors.size());
-                    Assertions.assertEquals("Bess Flowers (82)", mostHiredActors.get(0));
-                    Assertions.assertEquals("Christopher Lee (69)", mostHiredActors.get(1));
-                    Assertions.assertEquals("Grey Griffin (58)", mostHiredActors.get(2));
-                    Assertions.assertEquals("Danny Trejo (54)", mostHiredActors.get(3));
-                    Assertions.assertEquals("Eric Roberts (52)", mostHiredActors.get(4));
+                    assertEquals(5, mostHiredActors.size());
+                    assertEquals("Bess Flowers (82)", mostHiredActors.get(0));
+                    assertEquals("Christopher Lee (69)", mostHiredActors.get(1));
+                    assertEquals("Grey Griffin (58)", mostHiredActors.get(2));
+                    assertEquals("Danny Trejo (54)", mostHiredActors.get(3));
+                    assertEquals("Eric Roberts (52)", mostHiredActors.get(4));
 
-                    Assertions.assertEquals(2, mostAppearingGenres.size());
-                    Assertions.assertEquals("Drama (14495)", mostAppearingGenres.get(0));
-                    Assertions.assertEquals("Comedy (8955)", mostAppearingGenres.get(1));
+                    assertEquals(2, mostAppearingGenres.size());
+                    assertEquals("Drama (14495)", mostAppearingGenres.get(0));
+                    assertEquals("Comedy (8955)", mostAppearingGenres.get(1));
 
-                    Assertions.assertEquals("64.1 %female, 35.9 %male",maleFemaleRatio);
+                    assertEquals("64.1 %female, 35.9 %male", maleFemaleRatio);
                 }
         );
     }
